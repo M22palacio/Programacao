0d032cc05a2633268f5cbe4fa10ffa4d9701e0be
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-10-25 19:17:30 -0300

    Add files via upload

---
+++ PYTHON/Ex10.py
@@ -0,0 +1,9 @@
+# Mostrar o menor entre 2 números
+
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+
+if n1<n2:
+    print(f"O menor número é: {n1:.2f}")
+else:
+    print(f"O menor número é: {n2:.2f}")
