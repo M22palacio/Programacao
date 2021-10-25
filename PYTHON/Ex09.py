898657b5be2df3a4afb053de476a0882e58ab124
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-10-25 19:13:36 -0300

    Add files via upload

---
+++ PYTHON/Ex09.py
@@ -0,0 +1,10 @@
+# Mostrar o maior entre 2 números
+
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+
+if n1>n2:
+    print(f"O maior número é: {n1:.2f}")
+else:
+    print(f"O maior número é: {n2:.2f}")
+
