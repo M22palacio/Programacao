e600568178d6785461a0c264cd0b32035dc4940b
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-10-25 20:12:26 -0300

    Add files via upload

---
+++ PYTHON/Ex12.py
@@ -0,0 +1,11 @@
+# Se a área do retângulo for maior ou igual a 100, exibir Terreno Grande!
+print("Cálculo da área do retângulo")
+
+b = int(input('Digite o valor da base: '))
+h = int(input('Digite o valor da altura: '))
+
+area = b * h
+print(f"O valor da área é: {area}")
+
+if area >= 100:
+    print(f"Terreno grande!")
