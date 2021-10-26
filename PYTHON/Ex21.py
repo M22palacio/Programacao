6941c65f9a1ff4a0944b251dea9e929aad5307f5
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-10-26 16:57:24 -0300

    Add files via upload

---
+++ PYTHON/Ex21.py
@@ -0,0 +1,8 @@
+# Calcular a nota da P2 necessária para aprovação
+# media = (p1 + p2*2)/3
+print(f"Cálculo da nota P2 necessária para aprovação")
+
+p1 = float(input('Digite a nota da P1: ')
+
+ p2 = (5*3) - p1)/2
+  print(f"A Nota P2 necessária para aprovação é: {p2:.2f}")
