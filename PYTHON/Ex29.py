36615e3473e9f871f31a7c03beb3f9a3266ddec5
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-11-08 20:52:27 -0300

    Add files via upload

--- PYTHON/Ex29.py
+++ PYTHON/Ex29.py
@@ -1,7 +1,7 @@
 ''' Exibir a soma dos números inteiros positivos do intervalo de 1 a 100'''
 
 print("Exibir a soma dos números inteiros positivos do intervalo de 1 a 100")
-input("Pressione enter para continuar: ")
+input("Pressione enter para iniciar: ")
 
 res = int((1+100)*(100/2))
 print(f"O resultado da soma é: {res}")
