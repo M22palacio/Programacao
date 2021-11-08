4f987e09066a2d92d98fb1bbd3f625d244aea834
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-11-08 20:59:30 -0300

    Add files via upload

--- PYTHON/Ex31.py
+++ PYTHON/Ex31.py
@@ -7,7 +7,7 @@ f1=-1
 f2=1
 f3=1
 
-for i in range(1,21,1): #range ( parte do 1, vai até inteiro menor que 31, no passo 1 em 1 )  
+for i in range(1,21,1): #range ( parte do 1, vai até inteiro menor que 21, no passo 1 em 1 )  
     fn = f1+f2+f3
     print(f"{fn}")
     f3=f2
