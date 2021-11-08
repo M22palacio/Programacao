d8a66f1a2461d92d585f84d414234d1b7c85bf5c
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-11-08 20:58:46 -0300

    Add files via upload

---
+++ PYTHON/Ex31.py
@@ -0,0 +1,15 @@
+''' Exibir os vinte primeiros valores da série de Bergamaschi'''
+
+print("Exibir os vinte primeiros valores da série de Bergamaschi")
+input("Pressione enter para iniciar: ")
+
+f1=-1
+f2=1
+f3=1
+
+for i in range(1,21,1): #range ( parte do 1, vai até inteiro menor que 31, no passo 1 em 1 )  
+    fn = f1+f2+f3
+    print(f"{fn}")
+    f3=f2
+    f2=f1
+    f1=fn
