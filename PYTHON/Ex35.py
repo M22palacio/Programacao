6cb886ce8b008a6b766b3b4a2f79f394259d3652
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-11-09 11:43:10 -0300

    Add files via upload

--- PYTHON/Ex35.py
+++ PYTHON/Ex35.py
@@ -6,13 +6,13 @@ print("Entrar com 20 valores em um vetor, multiplicar por uma cte e exibir o res
 
 numeros = []
 
-for i in range(0,3,1): #range ( parte do 0, vai até inteiro menor que 20, no passo 1 em 1 )  
+for i in range(0,20,1): #range ( parte do 0, vai até inteiro menor que 20, no passo 1 em 1 )  
     num = int(input('Digite um número: '))
     numeros.append(num)
 
 cte = int(input('Digite o valor da constante: '))
 
 print('O vetor multiplicado pela constante é: ')
-for i in range (0,3,1): #varrendo o vetor
+for i in range (0,20,1): #varrendo o vetor
    res = cte*numeros[i]
    print(res)
