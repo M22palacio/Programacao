08ed511e3731226f66a6099a0b3eee82b718e0bb
M22palacio <89814288+M22palacio@users.noreply.github.com>
2021-11-09 11:25:18 -0300

    Add files via upload

--- PYTHON/Ex34.py
+++ PYTHON/Ex34.py
@@ -12,7 +12,7 @@ for i in range(0,10,1): #range ( parte do 0, vai até inteiro menor que 10, no p
 print('O maior número é: ')
 
 maior=numeros[0] #Considerando a posição [0] do vetor, como sendo a do maior número
-for i in range (0,10,1): #range (parte do 0, vai até inteiro menor que 10, no passo 1)
+for i in range (0,10,1): #varrendo o vetor
     if numeros[i]>maior: #Se algum número de outra posição, for maior que o número da posição[0]
      maior=numeros[i] #Atribui esse número como sendo o maior
 print(maior)
